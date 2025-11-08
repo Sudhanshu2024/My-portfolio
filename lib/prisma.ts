80da1f99380f9edbbe9c7eb36398ee9b654d9295
Sudhanshu Kumar Sinha <1ds20ec208@dsce.edu.in>
2025-11-08 17:28:11 +0530

    latest fixed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
+// Add type to global object
+const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    log: ['error', 'warn'], // remove 'query' for cleaner logs on prod
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
